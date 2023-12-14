a447bb01128a0934aa4b9e2564a77d449ee06482
rlagosg <rlagosg@yahoo.com>
2023-12-14 15:10:13 -0600

    update more movies

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -35,25 +34,81 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     super.initState();
     
     ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+    ref.read( popularMoviesProvider.notifier ).loadNextPage();
+    ref.read( topRatedMoviesProvider.notifier ).loadNextPage();
+    ref.read( upComingMoviesProvider.notifier ).loadNextPage();
   }
 
 
   @override
   Widget build(BuildContext context) {
 
-    final slideShowMovie = ref.watch( moviesSlideshowProvider );    
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );    
+    final initiialLoading = ref.watch(initialLoadingProvider);
+    if ( initiialLoading ) return const FullScreenLoader();
 
-    return Column(
-      children: [
-        const CustomAppbar(),
-        MoviesSlideShow(movies: slideShowMovie),
-        MovieHorizontalListview(
-          movies: nowPlayingMovies, 
-          title: 'En cines',
-          subTitle: 'Enero 20',
-        )
-      ],
+    final slideShowMovie = ref.watch( moviesSlideshowProvider );   
+
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );    
+    final popularMovies = ref.watch( popularMoviesProvider );    
+    final topRatedMovies = ref.watch( topRatedMoviesProvider );    
+    final upComingMovies = ref.watch( upComingMoviesProvider );    
+
+    return CustomScrollView(
+      slivers: [
+
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            titlePadding: EdgeInsets.all(5),
+            title: CustomAppbar(),
+          ),
+        ),
+
+        SliverList(delegate: SliverChildBuilderDelegate(
+          (context, index){
+            return Column(
+              children: [
+                //const CustomAppbar(),
+                MoviesSlideShow(movies: slideShowMovie),
+            
+                MovieHorizontalListview(
+                  movies: nowPlayingMovies, 
+                  title: 'En cines',
+                  subTitle: 'Enero 20',
+                  loadNextPage: ()=> ref.read(nowPlayingMoviesProvider.notifier).loadNextPage() ,
+                ),
+            
+                MovieHorizontalListview(
+                  movies: upComingMovies, 
+                  title: 'Proximamente',
+                  subTitle: 'En este mes',
+                  loadNextPage: ()=> ref.read(upComingMoviesProvider.notifier).loadNextPage() ,
+                ),
+
+                MovieHorizontalListview(
+                  movies: popularMovies, 
+                  title: 'Populares',
+                  subTitle: 'Con mas vistas',
+                  loadNextPage: ()=> ref.read(popularMoviesProvider.notifier).loadNextPage() ,
+                ),
+
+                MovieHorizontalListview(
+                  movies: topRatedMovies, 
+                  title: 'Mejor Calificadas',
+                  subTitle: 'Desde siempre',
+                  loadNextPage: ()=> ref.read(topRatedMoviesProvider.notifier).loadNextPage() ,
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+            
+              ],
+            );
+          },
+          childCount: 1
+        ))
+      ]
     );
   }
 } 
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,3 +1,4 @@
+export './shared/full_screen_loader.dart';
 export './movies/movies_slideshow.dart';
 export './movies/movie_horizontal_listview.dart';
 export './shared/custom_appbar.dart';
